a9d6a627261b09ef4e42bfd66d4bd67305dca973
Maximan97 <m4x14cun42024@gmail.com>
2025-06-04 23:33:49 -0300

    para terminar

---
+++ nivel_intermiedo/Projecto/clases.py
@@ -0,0 +1,15 @@
+from definiciones import agregar_libro, prestar_libro, librero
+from menu import *
+
+
+class Biblioteca():
+    pass
+
+
+class Libro():
+    def __init__(self, agregar_libro, prestar_libro):
+
+
+class Estante():
+    def estanteria(self):
+        self.estanteria = librero
