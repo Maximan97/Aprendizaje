3279e6b26935243faca1373bdfaab87c30108437
Maximan97 <m4x14cun42024@gmail.com>
2025-06-06 20:46:40 -0300

    para terminar

--- nivel_intermiedo/Projecto/clases.py
+++ nivel_intermiedo/Projecto/clases.py
@@ -1,15 +1,41 @@
-from definiciones import agregar_libro, prestar_libro, librero
-from menu import *
+librero = ["pito"]
 
 
 class Biblioteca():
+    # Tiene estantes
     pass
 
 
 class Libro():
-    def __init__(self, agregar_libro, prestar_libro):
+    def __init__(self, titulo, estado):
+        self.estado = estado
+        self.titulo = titulo
+    # Titulo
+    # Estado
+
+    def agregar_libro(self):
+        libro = input("Nombre del libro?: ")
+        librero.append(libro)
+        print(f"{libro} ya fue agregado al librero.")
+
+    def buscar_libro(self):
+        if libro in librero:
+            return True
+        else:
+            return False
+
+    def prestar_libro(self):
+        libro = input("Que libro te queres llevar?: ")
+        if buscar_libro(self, libro):
+            librero.remove(libro)
+            print("Ya lo tienes en el inventario")
+        else:
+            print("Te lo ganaron wey")
 
 
-class Estante():
-    def estanteria(self):
-        self.estanteria = librero
+class Estante(Libro):
+    # Tiene libros
+    librero = []
+
+    def __init__(self):
+        pass
