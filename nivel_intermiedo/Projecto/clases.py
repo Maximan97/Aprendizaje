c92a8c84acbcd127b8a5c0c9e7c09d14b350d7bf
Jamjize <jamjizelol@gmail.com>
2025-06-08 22:27:40 -0300

    leo check

--- nivel_intermiedo/Projecto/clases.py
+++ nivel_intermiedo/Projecto/clases.py
@@ -1,41 +1,60 @@
-librero = ["pito"]
+class Mostrador():
+    def menu(self) -> str:
+        """Muestra menú y retorna la opción elegida x usuario"""
+        print("Bienvenido a la Biblioteca.")
+        print("")
+        print("1. Agregar libro a alal.")
+        print("2. Pedir libro a la Biblioteca.")
+        print("3. Mostrar todos los libros disponibles.")
+        print("0. Salir")
+        print("")
+        opcion = input("Que opción eliges?: ")
+        return opcion
+
+
+class Estante():
+    """Tiene libros"""
+
+    def __init__(self):
+        self.librero = ["pito"]
 
 
 class Biblioteca():
-    # Tiene estantes
-    pass
+    def __init__(self):
+        self.mostrador = Mostrador()
+        self.estante = Estante()
+        self.librero = self.estante.librero
 
-
-class Libro():
-    def __init__(self, titulo, estado):
-        self.estado = estado
-        self.titulo = titulo
-    # Titulo
-    # Estado
+    def mostrar_menu(self):  # Muestra menú y pide opciones
+        while True:
+            opcion = self.mostrador.menu()
+            if opcion == "1":
+                self.agregar_libro()
+            elif opcion == "2":
+                self.prestar_libro()
+            elif opcion == "3":
+                print(self.librero)
+            elif opcion == "0":
+                print("Nos vemos.")
+                break
+            else:
+                print("No es una opción valida.")
 
     def agregar_libro(self):
-        libro = input("Nombre del libro?: ")
-        librero.append(libro)
-        print(f"{libro} ya fue agregado al librero.")
+        titulo = input("Nombre del libro?: ")
+        self.librero.append(titulo)
+        print(f"{titulo} ya fue agregado al librero.")
 
-    def buscar_libro(self):
-        if libro in librero:
+    def buscar_libro(self, titulo):
+        if titulo in self.librero:
             return True
         else:
             return False
 
     def prestar_libro(self):
-        libro = input("Que libro te queres llevar?: ")
-        if buscar_libro(self, libro):
-            librero.remove(libro)
+        titulo = input("Que libro te querés llevar?: ")
+        if self.buscar_libro(titulo):
+            self.librero.remove(titulo)
             print("Ya lo tienes en el inventario")
         else:
             print("Te lo ganaron wey")
-
-
-class Estante(Libro):
-    # Tiene libros
-    librero = []
-
-    def __init__(self):
-        pass
